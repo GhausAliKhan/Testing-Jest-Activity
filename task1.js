2f200967753909f0709cb3c17b6d3ad873cd987a
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-07-25 12:38:25 +0500

    Add Functions and Test for Task 1 to 4

---
+++ task1.js
@@ -0,0 +1,8 @@
+const stringLength = (string) => {
+  if (string.length < 1 || string.length > 10) {
+    throw new Error("string length should be between 1 and 10 characters");
+  }
+  return string.length;
+};
+
+module.exports = stringLength;
